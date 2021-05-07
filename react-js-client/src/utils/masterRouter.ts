e597c584fd4155b3f1f7f8f62e4c268a7b73786f
bakasa012 <51442823+bakasa012@users.noreply.github.com>
2021-05-07 16:07:20 +0700

    test client new

---
+++ react-js-client/src/utils/masterRouter.ts
@@ -0,0 +1,7 @@
+import { useContext } from "react";
+import {__RouterContext} from "react-router";
+
+// const masterRouter = ()=>useContext(__RouterContext);
+// export default masterRouter;
+
+export default ()=>useContext(__RouterContext);
